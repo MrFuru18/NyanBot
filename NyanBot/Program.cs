a6f810ee4fd6202046871c59b5b33deb067f8fcf
Wojciech Babiński <mrfuru@op.pl>
2024-04-12 22:04:34 +0200

    Initialization

---
+++ NyanBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NyanBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everynyan!!! UwU");
+        }
+    }
+}
